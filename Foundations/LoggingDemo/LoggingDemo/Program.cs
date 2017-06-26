ef5c165583bfa47052894bbfa96ed446c2139a78
Christian Nagel <christian@christiannagel.com>
2017-06-26 15:33:48 +0200

    logging sample

---
+++ Foundations/LoggingDemo/LoggingDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace LoggingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InitLogging();
+
+            Foo();
+
+        }
+
+        private static void Foo()
+        {
+            Logger.LogInformation($"{nameof(Foo)} started");
+            Bar("one");
+            Logger.LogInformation($"{nameof(Foo)} finished");
+        }
+
+        private static void Bar(string s)
+        {
+            Logger.LogError("some error in Bar");
+        }
+
+        static void InitLogging()
+        {
+            var loggerFactory = new LoggerFactory().AddConsole().AddDebug();
+
+            Logger = loggerFactory.CreateLogger<Program>();
+        }
+
+        public static ILogger Logger { get; private set; }
+    }
+}
