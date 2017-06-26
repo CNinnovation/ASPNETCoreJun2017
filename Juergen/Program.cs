121b4d975a3b1c1486cfdb0bb018b3052db212de
Juergen Wolfsteiner <juergen.wolfsteiner@ecx.io>
2017-06-26 13:23:55 +0200

    JWO: Lab 01

---
+++ Juergen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Juergen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
