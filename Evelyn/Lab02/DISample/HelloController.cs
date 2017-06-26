089e3fe8ee9e436a711fe468eaf0bccf26227543
Evelyn <evelyn.enzinger@brz.gv.at>
2017-06-26 14:13:49 +0200

    Lab02

---
+++ Evelyn/Lab02/DISample/HelloController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DISample
+{
+    class HelloController
+    {
+        private readonly IHelloService _HelloService;
+
+        public HelloController(IHelloService HelloService)
+        {
+            _HelloService = HelloService;
+        }
+
+        public string Greeting(string name) => _HelloService.Hello(name);
+
+    }
+}
