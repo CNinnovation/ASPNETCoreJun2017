c7c31bbff64711fee07a5cbeff2f399746d60faa
blog4you <hiersche@blog4you.at>
2017-06-26 14:14:36 +0200

    added new Files

---
+++ Hiersche_Markus/Lab2/IHelloService.cs
@@ -0,0 +1,7 @@
+﻿namespace Lab2
+{
+    public interface IHelloService
+    {
+        string Hello();
+    }
+}
--- Hiersche_Markus/Lab2/Program.cs
+++ Hiersche_Markus/Lab2/Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace Lab2
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var services = RegisterHelloServices();
+
+            HelloController c = services.GetService<HelloController>();
+
+            Console.WriteLine(c.Greeting());
+            Console.ReadKey();
+        }
+
+        public static IServiceProvider RegisterHelloServices()
+        {
+            ServiceCollection services = new ServiceCollection();
+
+            return services
+               .AddTransient<IHelloService, HelloService>()
+               .AddSingleton<HelloController>()
+               .BuildServiceProvider();
         }
     }
 }
