b411f3361a3e8bef94ce1b68d680c9103b80f0c1
Juergen Wolfsteiner <juergen.wolfsteiner@ecx.io>
2017-06-26 14:00:32 +0200

    JWO: Lab02 - created DI sample

---
+++ Juergen/Lab02/HelloController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab02
+{
+    public class HelloController
+    {
+        private IHelloService _service;
+
+        public HelloController(IHelloService service)
+        {
+            _service = service;
+        }
+
+        public string Greeting()
+        {
+           return _service.Hello();
+        }
+    }
+}
