70d0c44921114af468216c24d3b349311f9b0e3d
Juergen Wolfsteiner <juergen.wolfsteiner@ecx.io>
2017-06-27 16:39:53 +0200

    JWO: Lab 04 - implemented samples

---
+++ Juergen/Lab04/Models/Repos/BooksRepo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab04.Models;
+
+namespace Lab04.Repos
+{
+    public class BooksRepo : IBooksRepo
+    {
+        public List<Book> GetBooks()
+        {
+            List<Book> books = new List<Book>()
+            {
+                new Book() { Id = 1, Title = "Holy Bible" },
+                new Book() { Id = 2, Title = "Holy Bible V.2" }
+            };
+
+            return books;
+        }
+    }
+}
---
+++ Juergen/Lab04/Models/Repos/IBooksRepo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Lab04.Models;
+
+namespace Lab04.Repos
+{
+    public interface IBooksRepo
+    {
+        List<Book> GetBooks();
+    }
+}
