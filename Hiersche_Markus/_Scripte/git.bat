176031f688c9efe8ec06839a5f0cc01e302a541e
blog4you <hiersche@blog4you.at>
2017-06-27 08:42:07 +0200

    201706270842

--- Hiersche_Markus/_Scripte/git.bat
+++ Hiersche_Markus/_Scripte/git.bat
@@ -10,7 +10,15 @@ cd GitHub_Hiersche\ASPNETCoreJun2017
 git pull
 
 echo *** PUSH: ASP.Net Core 06 2017 ***
+set stunde=%TIME:~0,2%
+if "%stunde:~0,1%"==" " set stunde=0%stunde:~1,1%
+set minute=%TIME:~3,2%
+::set sekunde=%TIME:~6,2%
+set jahr=%date:~-4%
+set monat=%date:~-10,2%
+set tag=%date:~-7,2%
+set datetime=%jahr%%monat%%tag%%stunde%%minute%
 cd Hiersche_Markus
 git add .
-git commit -m "..."
+git commit -m "%datetime%"
 git push
