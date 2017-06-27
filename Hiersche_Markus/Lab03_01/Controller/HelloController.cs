9ff1173d6640a487363f8c8520b9eb307cd3b74a
blog4you <hiersche@blog4you.at>
2017-06-27 14:07:26 +0200

    2017.06.27_14:07

---
+++ Hiersche_Markus/Lab03_01/Controller/HelloController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab03_01.Services;
+
+namespace Lab03_01.Controller
+{
+    public class HelloController
+    {
+        private readonly IGreetingService _greetingService;
+        public HelloController(IGreetingService greetingService)
+        {
+            _greetingService = greetingService;
+        }
+        public string Index(string name) => _greetingService.Greet(name);
+    }
+}
---
+++ Hiersche_Markus/Lab03_01/Services/IGreetingService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab03_01.Services
+{
+    public interface IGreetingService
+    {
+        string Greet(string name);
+    }
+}
