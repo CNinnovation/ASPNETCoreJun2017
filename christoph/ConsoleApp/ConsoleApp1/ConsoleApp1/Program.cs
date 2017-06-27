f3c8b97eae86ed382f3023783f99856eb80a8088
skladal <c.skladal@gmail.com>
2017-06-27 14:33:36 +0200

    new

--- christoph/ConsoleApp/ConsoleApp1/ConsoleApp1/Program.cs
+++ christoph/ConsoleApp/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ConsoleApp1
 {
@@ -6,7 +7,35 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RegisterServices();
+            //Console.WriteLine("Hello World!");
+            UseDependencyInjection();
+            UseDependencyInjectionWithContainer();
+            Console.ReadLine();
         }
+        private static void UseDependencyInjection()
+        {
+            IHelloService helloService = new MyHelloService();
+            var greetingController = new GreetingController(helloService);
+            string result = greetingController.MyWebGreeting("Katharina");
+            Console.WriteLine(result);
+
+        }
+        private static void UseDependencyInjectionWithContainer()
+        {
+            var greetingController = Container.GetService<GreetingController>();
+            string result = greetingController.MyWebGreeting("Stephanie");
+            Console.WriteLine(result);
+        }
+
+        public static void RegisterServices()
+        {
+            var services = new ServiceCollection();
+            services.AddSingleton<IHelloService, MyHelloService>();    
+            services.AddTransient<GreetingController>();
+            Container = services.BuildServiceProvider();
+        }
+        public static IServiceProvider Container { get; private set; }
+
     }
 }
