d52dbb620a106dd8f9d8ba70647beb034b5dc67c
blog4you <hiersche@blog4you.at>
2017-06-27 09:49:57 +0200

    2017.06.27_09:49

---
+++ Hiersche_Markus/Lab2_config/MyTypedSetting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab2_config
+{
+    public struct Zahl
+    {
+        public int b { get; set; }
+    }
+
+    class MyTypedSetting
+    {
+        public Zahl zahl { get; set; }
+        public string title { get; set; }
+    }
+}
---
+++ Hiersche_Markus/Lab2_config/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Lab2_config
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadConfig();
+        }
+
+        private static void ReadConfig()
+        {
+            //string str = Configuration.GetSection("MyConfig")["a"];
+            //Console.WriteLine("Setting \"a\": " + str + "\n");
+
+            MyTypedSetting setting = new MyTypedSetting();
+            Configuration.GetSection("MyTypedSetting").Bind(setting);
+            Console.WriteLine(setting.title + " " + setting.zahl.b);
+
+            Console.ReadKey();
+        }
+
+        public static IConfigurationRoot Configuration { get; private set; }
+    }
+}
