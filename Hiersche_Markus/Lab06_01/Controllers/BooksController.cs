4656f6abcf342736e57283d63c755094621ff9d2
blog4you <hiersche@blog4you.at>
2017-06-28 13:19:57 +0200

    2017.06.28_13:19

---
+++ Hiersche_Markus/Lab06_01/Controllers/BooksController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Lab06_01.Models;
+
+namespace Lab06_01.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET /Books/Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Hiersche_Markus/Lab06_01/Models/Book.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab06_01.Models
+{
+    public class Book
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        private string publisher;
+
+        public string Publisher
+        {
+            get { return publisher; }
+            set { publisher = value; }
+        }
+
+        public Book(int id, string title, string publisher)
+        {
+            this.Id = id;
+            this.Title = title;
+            this.Publisher = publisher;
+        }
+    }
+}
