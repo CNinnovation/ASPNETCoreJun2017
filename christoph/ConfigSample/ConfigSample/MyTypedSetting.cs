4026fa1e40f408748bd5c874c054dcabb84b34ba
skladal <c.skladal@gmail.com>
2017-06-28 08:04:41 +0200

    changed

---
+++ christoph/ConfigSample/ConfigSample/MyTypedSetting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConfigSample
+{
+    public struct Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    public class MyTypedSetting
+    {
+        public Point TopLeft { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ christoph/LoggingDemo/LoggingDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace LoggingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InitLogging();
+
+            Foo();
+        }
+
+        private static void Foo()
+        {
+            Logger.LogInformation($"{nameof(Foo)} started");
+            Bar("one");
+            Logger.LogInformation($"{nameof(Foo)} finished");
+        }
+
+        private static void Bar(string s)
+        {
+            Logger.LogError("Some error in Bar");
+        }
+
+        static void InitLogging()
+        {
+            var loggerFactory = new LoggerFactory().AddConsole().AddDebug();
+            Logger = loggerFactory.CreateLogger<Program>();
+        }
+
+        public static ILogger Logger { get; private set; }
+    }
+}
