8d77efb83d89acf6bf91542b781498abcd49bed6
Christian Nagel <christian@christiannagel.com>
2017-06-28 16:15:21 +0200

    model binding with ASP.NET MVC Core

---
+++ aspnetcoremvc/SimpleMVCApp/SimpleMVCApp/Controllers/AnotherBooksController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleMVCApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleMVCApp.Controllers
+{
+    public class AnotherBooksController : Controller
+    {
+        public IActionResult Book()
+        {
+            return View(new Book { Title = "init", Publisher = "init" });
+        }
+
+        [HttpPost]
+        public IActionResult Book(Book book)
+        {
+            return View();
+        }
+    }
+}
