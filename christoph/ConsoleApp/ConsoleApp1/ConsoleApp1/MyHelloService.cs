699117048ec6978f5e65bba1b94b93b1f62b961a
skladal <c.skladal@gmail.com>
2017-06-28 08:03:38 +0200

    new files

---
+++ christoph/ConsoleApp/ConsoleApp1/ConsoleApp1/MyHelloService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class MyHelloService : IHelloService
+    {
+        public string Hello(string name) => $"Hello, {name}";
+        //public string Hello(string name)
+        //{
+        //    return String.Format("Hello, {0}", name);
+        //}
+    }
+}
