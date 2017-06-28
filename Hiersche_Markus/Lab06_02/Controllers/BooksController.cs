51182f86021953aa982c68b0323e539f15c2b518
blog4you <hiersche@blog4you.at>
2017-06-28 16:58:51 +0200

    2017.06.28_16:58

---
+++ Hiersche_Markus/Lab06_02/Controllers/BooksController.cs
@@ -0,0 +1,35 @@
+﻿using Lab06_02.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab06_02.Controllers
+{
+    public class BooksController
+    {
+        private readonly BooksContext _context;
+
+        public BooksController(BooksContext context)
+        {
+            this._context = context;
+        }
+
+        // GET /Books/Index
+        public IActionResult Index()
+        {
+            //_context.Add(new boo)+
+            _context.Books.Add(new Book("abc1", "Abc1"));
+            _context.Books.Add(new Book("abc2", "Abc2"));
+            _context.Books.Add(new Book("abc3", "Abc3"));
+            _context.Books.Add(new Book("abc4", "Abc4"));
+            var LoBooks = from b in _context.Books
+                          select b;
+
+
+            //ViewBag.Books = LoBooks;
+            return View(LoBooks);
+        }
+    }
+}
---
+++ Hiersche_Markus/Lab06_02/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab06_02.Models
+{
+    public class Book
+    {
+        public static int anzahl = 0;
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Publisher { get; set; }
+
+        public Book(string Title = "", string Publisher = "")
+        {
+            anzahl++;
+            Id = anzahl;
+            this.Title = Title;
+            this.Publisher = Publisher;
+        }
+    }
+}
