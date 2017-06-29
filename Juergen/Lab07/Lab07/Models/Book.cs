6a3809b73511be17305e3685ade9b8d5f037d428
Juergen Wolfsteiner <juergen.wolfsteiner@ecx.io>
2017-06-29 13:57:00 +0200

    JWO: Lab07 - Used Html and Tag-Helper, created Barcode-HtmlHelper, created rdmColor-TagHelper

---
+++ Juergen/Lab07/Lab07/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab06.Models
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Publisher { get; set; }
+
+        public string Author { get; set; }
+    }
+}
